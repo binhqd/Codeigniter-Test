0eba4576f8ade726ed42362b0eec4d0a5b8c61f7
Binh Quan <binhqd@gmail.com>
2015-01-30 16:15:12 +0700

    Add code doc

--- application/models/ipadtest_model.php
+++ application/models/ipadtest_model.php
@@ -8,19 +8,37 @@ class IpadTest_model extends CI_Model
         parent::__construct();
     }
 
+    /**
+     * This method is used to output message for testing
+     *
+     * @param string $message            
+     * @param string $tag            
+     * @param string $debug            
+     */
     private static function describe($message, $tag = "p", $debug = true)
     {
         if ($debug) {
             echo "<{$tag}>{$message}</{$tag}>";
         }
     }
-    
-    private static function extra($no) {
-        if ($no == 1) return "1st";
-        if ($no == 2) return "2nd";
-        if ($no == 3) return "3rd";
+
+    /**
+     * This is an utility method to display the correct text string
+     *
+     * @param int $no            
+     * @return string
+     */
+    private static function extra($no)
+    {
+        if ($no == 1)
+            return "1st";
+        if ($no == 2)
+            return "2nd";
+        if ($no == 3)
+            return "3rd";
         return "{$no}th";
     }
+
     /**
      * This method is used to test the highest storey that the ipad still survive
      *
@@ -42,11 +60,11 @@ class IpadTest_model extends CI_Model
             // determine current storey number
             $storeyNo = $i * $noIpads;
             
-            self::describe("<hr/><u>".self::extra($n)." drop</u>. Perform test at ".self::extra($storeyNo)." storey.", "p", $describe);
+            self::describe("<hr/><u>" . self::extra($n) . " drop</u>. Perform test at " . self::extra($storeyNo) . " storey.", "p", $describe);
             
             // If first item is broken
             if ($storeyNo > $result) {
-                self::describe("<hr/><span style='color:red'>First item is broken at ".self::extra($storeyNo)." storey.</span>", "p", $describe);
+                self::describe("<hr/><span style='color:red'>First item is broken at " . self::extra($storeyNo) . " storey.</span>", "p", $describe);
                 // number of item remaining after the first item is broken
                 $tries = $noIpads - 1;
                 
@@ -57,10 +75,10 @@ class IpadTest_model extends CI_Model
                     $n ++;
                     
                     $storeyNo --; // go down 1 storey
-                    self::describe("<u>" . self::extra($n)." drop</u>. Go down to ".self::extra($storeyNo)." storey and drop 1 item", "p", $describe);
+                    self::describe("<u>" . self::extra($n) . " drop</u>. Go down to " . self::extra($storeyNo) . " storey and drop 1 item", "p", $describe);
                     
                     if ($storeyNo == $result) { // until storey number is the max number that item still survive
-                        self::describe("<span style='color:blue'>Good at ".self::extra($storeyNo)." storey.</span>", "p", $describe);
+                        self::describe("<span style='color:blue'>Good at " . self::extra($storeyNo) . " storey.</span>", "p", $describe);
                         break; // Stop any loop
                     }
                     self::describe("Ipad is broken again", "p", $describe);
@@ -70,6 +88,7 @@ class IpadTest_model extends CI_Model
                 
                 // If
                 if ($tries == 0) {
+                    self::describe("<span style='color:red'>No item left</span>", "i", $describe);
                     break;
                 }
                 break;
